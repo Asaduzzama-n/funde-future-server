5cf4b375702c56ccfd91cb44863a8c7ce41784d4
Asaduzzaman <masaduzzaman193146@bscse.uiu.ac.bd>
2023-04-06 00:38:01 +0600

    st

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const nodemailer = require("nodemailer");
+const mg = require('nodemailer-mailgun-transport');
+
 require('dotenv').config();
+const stripe = require("stripe")("sk_test_51MsoefLLZ1o50Tb07lZcDPmFuo1mVf4QDfl7Yy8imWJ14fltPh1SM42yrilwK3G4NMJdoGfwoRAhbgd6XRDtyJMP00QtCpZuIp");
 
 const port = process.env.PORT || 5000;
+
 
 
 app.use(cors());
@@ -15,31 +20,57 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
-async function run(){
-    try{
+
+function sendInvoiceEmail(donation) {
+
+    const {donor_mail,amount,transactionId,campaign_id} = donation;
+
+    const auth = {
+        auth: {
+          api_key: process.env.EMAIL_SEND_KEY,
+          domain: process.env.EMAIL_DOMAIN
+        }
+      }
+      
+      const transporter = nodemailer.createTransport(mg(auth));
+
+      transporter.sendMail({
+        from: "asaduzzaman193146@gmail.com", // verified sender email
+        to: donor_mail || 'asadshanto310@gmail.com', // recipient email
+        subject: `Thank your for your donation`, // Subject line
+        text: "Hello world!", // plain text body
+        html: `
+        <div>
+            <p>You have donated ${amount} for campaign id: ${campaign_id} </p>
+            <p>Transaction ID:  ${transactionId}</p>
+        </div>
+        
+        `, // html body
+    }, function (error, info) {
+        if (error) {
+            console.log('Email send error', error);
+        } else {
+            console.log('Email sent: ' + info);
+        }
+    });
+
+}
+
+
+async function run() {
+    try {
         const campaignCollection = client.db("fund-future").collection('Campaigns');
         const donationCollection = client.db("fund-future").collection('Donation');
+        const storyCollection = client.db("fund-future").collection('Story');
 
 
-        //JWT
-        app.get('/my-campaigns',async(req,res)=>{
 
-            const query = {campaigner_mail: req.query.email}
-            const campaigns = await campaignCollection.find(query).toArray();
-
-            const donation_query ={donor_mail: req.query.email}
-            const donations = await donationCollection.find(donation_query).toArray();
-
-            
-            
-
-            res.send(campaigns);
-        })
-
-
-        //this two will be merged later using conditional query
-        app.get('/campaigns',async(req,res)=>{
-            const query = {}
+        //loading all campaign and campaign based on email ... used in campaigns and dashboard ---> myCampaigns
+        app.get('/campaigns', async (req, res) => {
+            let query = {}
+            if (req.query.email) {
+                query = { campaigner_mail: req.query.email }
+            }
             const cursor = campaignCollection.find(query);
             const campaigns = await cursor.toArray();
             res.send(campaigns);
@@ -48,32 +79,49 @@ async function run(){
 
 
 
+        //getting campaign with donation based on campaigner mail and donor mail
+        // app.get('/v2/campaigns',async(req,res)=>{
+        //     const campaignsWithDonation = await campaignCollection.aggregate([
+        //         {
+        //             $lookup:{
+        //                 from:'Donation',
+        //                 localField: 'campaigner_mail',
+        //                 foreignField: 'donor_mail',
+        //                 as:'donations'
+        //             }
+        //         }
+        //     ]).toArray();
+
+        //     res.send(campaignsWithDonation);
+        //     console.log(campaignsWithDonation);
+        // })
 
 
 
-        app.get('/campaign/:id',async(req,res)=>{
+
+
+        app.get('/campaign/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const campaign = await campaignCollection.findOne(query);
             res.send(campaign);
         })
 
         //Get featured campaign
-        app.get('/campaigns-featured',async(req,res)=>{
-            const query = {category: 'featured'}
+        app.get('/campaigns-featured', async (req, res) => {
+            const query = { category: 'featured' }
             const campaigns = await campaignCollection.find(query).toArray();
 
             res.send(campaigns)
-   
+
         })
 
-        //JWT get donation base on user email --> used in MyDonation
-        app.get('/donations',async(req,res)=>{
+        //JWT get all donation and donation based on user email --> used in MyDonation
+        app.get('/donations', async (req, res) => {
             let query = {}
-            if(req.query.email){
-                 query = {donor_mail: req.query.email}
+            if (req.query.email) {
+                query = { donor_mail: req.query.email }
             }
- 
             const donations = await donationCollection.find(query).toArray();
             res.send(donations);
         })
@@ -81,21 +129,39 @@ async function run(){
 
 
         //Get donation by campaign id
-        app.get('/donation/:id',async(req,res)=>{
+        app.get('/donation/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {campaign_id: id}
-            const cursor =  donationCollection.find(query);
+            const query = { campaign_id: id }
+            const cursor = donationCollection.find(query);
             const donation = await cursor.toArray();
             res.send(donation);
 
         })
 
 
+        //Get success stories
+        app.get('/successStories',async(req,res)=>{
+            const query = {};
+            const stories =await storyCollection.find(query).toArray();
+            res.send(stories);
+        })
+
+        //Get success story
+
+        app.get('/successStory/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const story =await storyCollection.findOne(query);
+            res.send(story);
+        })
 
 
-        //Post APIs
 
-        app.post('/campaigns',async(req,res)=>{
+
+
+        //Post APIs ---------------------------------\'''
+
+        app.post('/campaigns', async (req, res) => {
             const campaign = req.body;
             console.log(campaign);
             const result = await campaignCollection.insertOne(campaign);
@@ -104,9 +170,45 @@ async function run(){
         })
 
 
+        //Stripe  donation Intent--->
+        app.post('/create-payment-intent', async (req, res) => {
+            const donation = req.body;
+            const price = donation.d_amount;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+        //Donation information post---> donation.js, checkoutForm.js
+
+        app.post('/checkout', async (req, res) => {
+            const donation = req.body;
+            const result = await donationCollection.insertOne(donation);
+            //Send email----> invoice
+            sendInvoiceEmail(donation);
+            res.send(result);
+        })
+
+        //Success Story post------> createStory.js
+        app.post('/successStory',async(req,res)=>{
+            const story = req.body;
+            const result = await storyCollection.insertOne(story);
+            res.send(result);
+        })
+
+
     }
-    finally{
-    
+    finally {
+
     }
 
 }
@@ -120,6 +222,6 @@ run().catch(err => console.error(err));
 
 
 
-app.listen(port,(port, ()=>{
+app.listen(port, (port, () => {
     console.log("SERVER IS RUNNING");
 }))
