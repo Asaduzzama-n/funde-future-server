ef763a4bb7c364e8ef3c24cadaf12920aaa0f7eb
Asaduzzaman <masaduzzaman193146@bscse.uiu.ac.bd>
2023-04-24 00:23:06 +0600

    after charity

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 function sendInvoiceEmail(donation) {
 
-    const { donor_mail, amount, transactionId, campaign_id, campaign_name, donor_name } = donation;
+    const { donor_mail, amount, transactionId, campaign_id, campaign_name, donor_name,charity_name } = donation;
 
 
     let config = {
@@ -53,7 +53,7 @@ function sendInvoiceEmail(donation) {
             table: {
                 data: [
                     {
-                        campaign: campaign_name,
+                        campaign: campaign_name || charity_name,
                         description: "Thank you for your donation. Wish you all the best!",
                         amount: amount,
                     }
@@ -68,7 +68,7 @@ function sendInvoiceEmail(donation) {
     let message = {
         from: process.env.GOOGLE_EMAIL,
         to: donor_mail,
-        subject: `You have donated ${amount} to ${campaign_name}`,
+        subject: `You have donated ${amount} to ${campaign_name || charity_name}`,
         html: mail
     }
 
@@ -81,8 +81,6 @@ function sendInvoiceEmail(donation) {
     })
 
 }
-
-
 
 
 
@@ -180,6 +178,8 @@ async function run() {
 
         //Post APIs ---------------------------------\'''
 
+
+
         //user save
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -264,6 +264,16 @@ async function run() {
             res.send(result);
         })
 
+
+        //send mail to campaigner --->ContactModal.js
+        app.post('/sendEmail', async (req, res) => {
+            const contactMail = req.body;
+            // const result = await donationCollection.insertOne(donation);
+            //Send email----> invoice
+            sendInvoiceEmail(donation);
+            res.send(result);
+        })
+
         //Success Story post------> createStory.js
         app.post('/successStory', async (req, res) => {
             const story = req.body;
@@ -279,6 +289,61 @@ async function run() {
             const result = await storyCollection.deleteOne(query);
             res.send(result);
         })
+
+
+
+
+
+
+        const charities = [
+            {
+                _id: '1',
+                title: "As-Sunnah Foundation",
+                charity_desc: "As-Sunnah Foundation is a non-profit, non-political, and entirely charitable organization dedicated to human welfare. Following the ideals and footsteps of the teacher of humanity, liberator of mankind, and role model of generosity Prophet Muhammad (Saw), this organization is engaged in social reform, inculcation of great morality, establishing employment, poverty alleviation, low cost or free health care, expansion of Islamic teachings and culture, conducting multidisciplinary education projects, continuous program in building a clean mindset, above all using oral, written and modern media to make people obey Allah and abide by His Messenger (Saw).",
+                charity_img: "https://www.ourislam24.com/wp-content/uploads/2021/06/as-sunnah-logo.jpg",
+                charity_contact: "+88-09610-001089",
+                charity_email: "assunnahfoundationbd@gmail.com",
+                charity_campaigns:['Zakat','Orphan Fund','Sadaqah Jariyah Fund']
+            },
+            {
+                _id: '2',
+                title: "Bidyanondo foundation",
+                charity_desc: "A passionate group of people who wishes to inspire a nation and beyond. Bidyanondo is an educational voluntary organization that originated from Bangladesh. The official registration No. is S-12258/2015). Bidyanondo has been operated by 40 professionals and with help of thousands of volunteers. Our mission is to globally foster philanthropic works. By designing innovative and inspiring programs, our goal is to engage students and professionals to work together to educate, empower and lift the underprivileged. One of our endeavors is to provide free education, educational materials and support. We have established libraries and a printing press for original publications. Our motto is to teach with compassion where a good human being is developed rather than a typical human who is only professionally equipped. Starting from volunteers, their children, refugees, street children and orphans, anyone with an underprivileged background is welcome at our door. One of the highly successful projects that we carried out is the Ak-Taka-Ahar (One-taka-meal) program. Here we let people come in and purchase a meal for only 1 taka/0.012 USD. This guarantees entitlement to food while feeling self-reliance by purchasing with a minimum cost and at the same time having a decent, healthy meal with dignity. Under this program, we also provide food for people under the age of 12 and over 60 years old people. This successful venture has inspired us to launch One-Taka-Bazaar where any item sold is just 1 taka/ 0.012 USD and 1 taka Chikistsha ( One-taka-treatment). Anyone who has a need can come to get service just by paying 1 taka and we supplement the rest by crowdfunding.",
+                charity_img: "https://upload.wikimedia.org/wikipedia/en/2/23/Bidiyanondo_Foudition_logo.svg",
+                charity_contact: "+88-09610-001089",
+                charity_email: "assunnahfoundationbd@gmail.com",
+                charity_campaigns:['এক টাকায় আহার - 1 Taka Meal','সম্বল - Sombol','বিদ্যানন্দ অনাথালয়']
+
+            },
+            {
+                _id: '3',
+                title: "Action Aid bangladesh",
+                charity_desc: "ActionAid Bangladesh is working to combat against gender-based violence awareness about women sexual reproductive health rights. Women’s empowerment is key to the social, cultural and economic freedom of all citizens. Our engagement focuses on empowering women to fight for their rights. We will advocate for implementation of feminist policies to combat the injustice and inequality created by visible and invisible power structures in society.",
+                charity_img: "https://new-media.dhakatribune.com/en/uploads/2021/10/23/actionaid.jpeg",
+                charity_contact: "+88-09610-001089",
+                charity_email: "assunnahfoundationbd@gmail.com",
+                charity_campaigns:['EMPOWERING SPORTS WOMEN: A CELEBRATION OF FIFA WORLD CUP 2018','SHORT FILM MAKING COMPETITION ON UNPAID CARE WORK','SAFE CITY']
+
+            }
+        ]
+
+
+
+
+        app.get('/charity',(req,res)=>{
+            res.send(charities);
+        })
+
+
+        app.get('/charity/:id',(req,res)=>{
+            const id = req.params.id;
+            const charity = charities.find(charity => charity._id === id);
+            res.send(charity);
+        })
+
+
+
+
 
 
     }
